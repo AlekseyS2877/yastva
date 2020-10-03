1a3808acf631ac81811e010064b8cfaae04c612d
Aleksey Sokolov <alexis.sokolov@yahoo.com>
2020-10-03 17:50:29 +0300

    Начальная загрузка

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+   return (
+      <div className="App container-fluid">
+         <h1 className="display-1">Привет!</h1>
+      </div>
+   );
+}
+
+export default App;
