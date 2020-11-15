16be2c3bebd12f270dd28c4a607a1dc01002c4cb
Sibiria78 <juri.meier@gmx.de>
2020-11-15 22:27:40 +0100

    added logo and a food image

--- src/components/GoodsSmallCard.jsx
+++ src/components/GoodsSmallCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GoodsSmallCard = ({ goods }) => {
    return (
       <div className="card">
-         <img className="card-img-top" src="..." alt="test" />
+         <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Cooked_snails.JPG/1200px-Cooked_snails.JPG" alt="test" />
          <div className="card-body">
             <h5 className="card-title">{goods.name}</h5>
             <p className="card-text">{`Арт.${goods.code} Бренд: ${goods.brand}`}</p>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import StoreLogo from "./pics/jastva.jpg";
 
 function Header() {
-   const activeStyle = { color: "orange" };
-   return (
+  const activeStyle = { color: "orange" };
+  return (
+    <div>
+      <div className="logo">
+        <img src={StoreLogo} width="150" height="75" alt="Logo" />
+      </div>
       <nav>
-         <NavLink activeStyle={activeStyle} exact to="/">
-            Home
-         </NavLink>
-         {" | "}
-         <NavLink activeStyle={activeStyle} to="/goods">
-            Наши яства
-         </NavLink>
-         {" | "}
-         <NavLink activeStyle={activeStyle} to="/about">
-            О нас
-         </NavLink>
+        <NavLink activeStyle={activeStyle} exact to="/">
+          Home
+        </NavLink>
+        {" | "}
+        <NavLink activeStyle={activeStyle} to="/goods">
+          Наши яства
+        </NavLink>
+        {" | "}
+        <NavLink activeStyle={activeStyle} to="/about">
+          О нас
+        </NavLink>
       </nav>
-   );
+    </div>
+  );
 }
 
 export default Header;
